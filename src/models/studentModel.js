44c031025bd4d40f9cb8a4ee71a7ab2ceb3bac93
gobiananthx <gobiananthx@gmail.com>
2025-05-28 18:06:02 +0530

    add studentModel

--- src/models/studentModel.js
+++ src/models/studentModel.js
@@ -1,1 +1,24 @@
 // Student Model
+import mongoose from "mongoose";
+
+const studentSchema = new mongoose.Schema(
+  {
+    regNo: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Student", studentSchema);
